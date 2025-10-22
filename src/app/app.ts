8042b85ad70b7b507ebce0425b69ab9f3fcee551
dlares2000 <dlares@Daniels-MacBook-Pro.local>
2025-10-22 15:10:15 -0700

    Initial commit of Angular date selector project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+
+// Material Imports
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'; 
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core'; 
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatIconModule } from '@angular/material/icon'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule, 
+    MatDatepickerModule,
+    MatNativeDateModule, 
+    MatButtonModule, 
+    MatIconModule
+  ],
 })
-export class App {
-  protected title = 'my-datepicker';
+export class AppComponent {
+  range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
+
+  clearDates() {
+    this.range.setValue({ start: null, end: null });
+  }
 }
